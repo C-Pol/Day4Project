79c9143d3197b1cef11dc574cfe4e0cbac6ffe04
C-Pol <cpol.hayat@gmail.com>
2021-05-25 19:30:08 -0400

    first commit on review 1 branch

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,5 +1,11 @@
 public class BranchingPractice {
     public static void main(String[] args) {
+//created new project
+        //enabled VC
+        //ADDED .gitignore with .idea, target ,*.iml
+        //made initial commit by keeping track of all files
+        //shared project on github
 
+        //reviewing on new branch
     }
 }
